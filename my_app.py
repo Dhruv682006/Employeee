daa7030a92758ea3721ba522217ac6b4dc1a9663
Dhruv682006 <dhruvbhatt406@gmail.com>
2024-08-09 11:47:42 +0530

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -56,12 +56,68 @@ st.video("https://youtu.be/R3SHDFssDwg?si=iXDV7HWPbzOXiJre")
 st.video("https://youtu.be/yWAA2Y9bZ40?si=SNswYVCzRsNlvCze")
 
 
+if st.checkbox("checkbox"):
+    st.text("checkbox selection")
+#------------today--------------#
+
+radio_but = st.radio("Youe Selection", ["Male","Female"])
+if radio_but == "Male":
+    st.info("you are Male")
+else:
+    st.info("you are Female")
+
+    
+City = st.selectbox("your City", ["Dman","Div","Valsad"])
+if City == "Daman":
+    st.info("I love Daman")
+elif City == "Div":
+    st.info("I love Div")
+else:
+    st.info("I love Valsad")
+    
+
+#--------Pandas DataFrame----------#
+import streamlit as st
+import pandas as pd
+
+auto_data= pd.read_csv("auto.csv")
+st.dataframe(auto_data.head())
+
+st.table(auto_data.head(10))
+
+st.area_chart(auto_data[["mpg","cylinders"]])
+st.area_chart(auto_data[["mpg","cylinders"]].head(20))
+
+st.bar_chart(auto_data[["mpg","cylinders"]])
+st.bar_chart(auto_data[["mpg","cylinders"]].head(20))
 
 
+st.line_chart(auto_data[["mpg","cylinders"]])
+st.line_chart(auto_data[["mpg","cylinders"]].head(20))
 
+import datetime
+import time
 
+today = st.date_input("Today is",datetime.datetime.now())
+hour = st.time_input("The time is",datetime.time(12,30))
 
+st.code("import pandas as pd")
+st.code("print(WeLcome to NIELIT Daman)")
 
+import pandas as pd 
+import numpy as np
+
+st.title("Area")
+df=pd.DataFrame(np.random.randn(40,4),columns=["c1","c2","c3","c4"])
+st.bar_chart(df)
+
+st.title("Line chart")
+df=pd.DataFrame(np.random.randn(40,4),columns=["c1","c2","c3","c4"])
+st.line_chart(df)
+
+st.title("Area")
+df=pd.DataFrame(np.random.randn(40,4),columns=["c1","c2","c3","c4"])
+st.area_chart(df)
 
 
 
